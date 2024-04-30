d89b4f89c47f550d6028ffab77d169a4c84b09ee
Logan Thimer <thimerlogan@gmail.com>
2024-04-30 10:32:46 -0600

    Added styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -8,10 +8,95 @@ const roboto = Roboto({
   display: "swap",
 });
 
+const freshCleanTheme = {
+  primary: {
+    main: "#4CAF50",
+  },
+  secondary: {
+    main: "#FFEB3B",
+  },
+  background: {
+    default: "#FFFFFF",
+  },
+  text: {
+    primary: "#333333",
+  },
+  info: {
+    main: "#2196F3",
+  },
+};
+
+const modernSleekTheme = {
+  primary: {
+    main: "#607D8B",
+  },
+  secondary: {
+    main: "#FFC107",
+  },
+  background: {
+    default: "#FAFAFA",
+  },
+  text: {
+    primary: "#424242",
+  },
+  info: {
+    main: "#E91E63",
+  },
+};
+
+const earthyWarmTheme = {
+  primary: {
+    main: "#795548",
+  },
+  secondary: {
+    main: "#FF9800",
+  },
+  background: {
+    default: "#FFF3E0",
+  },
+  text: {
+    primary: "#3E2723",
+  },
+};
+
+const coolCalmTheme = {
+  primary: {
+    main: "#009688",
+  },
+  secondary: {
+    main: "#00BCD4",
+  },
+  background: {
+    default: "#E0F7FA",
+  },
+  text: {
+    primary: "#263238",
+  },
+};
+
 const theme = createTheme({
   typography: {
     fontFamily: roboto.style.fontFamily,
+    h1: {
+      fontSize: "4.5rem",
+    },
+    h2: {
+      fontSize: "3.75rem",
+    },
+    h3: {
+      fontSize: "3rem",
+    },
+    h4: {
+      fontSize: "2.125rem",
+    },
+    h5: {
+      fontSize: "1.5rem",
+    },
+    h6: {
+      fontSize: "1.25rem",
+    },
   },
+  palette: modernSleekTheme,
 });
 
 export default theme;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,25 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        // primary: "#4CAF50", // Green
+        // secondary: "#FFEB3B", // Yellow
+        // background: "#FFFFFF", // White
+        // accent: "#2196F3", // Blue
+        // text: "#333333", // Dark Gray
+
+        primary: "#607D8B", // Blue Gray
+        secondary: "#FFC107", // Amber
+        background: "#FAFAFA", // Off White
+        accent: "#E91E63", // Pink
+        text: "#424242", // Darker Gray
+
+        // primary: '#009688', // Teal
+        // secondary: '#00BCD4', // Cyan
+        // background: '#E0F7FA', // Pale Cyan
+        // accent: '#006064', // Dark Cyan
+        // text: '#263238', // Blue Gray
+      },
     },
   },
   plugins: [],
