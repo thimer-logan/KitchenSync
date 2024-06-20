ee2cf12f0271a0c0c52fae912c06b2ec494ef315
Logan Thimer <thimerlogan@gmail.com>
2024-06-20 06:58:31 -0600

    Added login and logout

---
+++ src/app/login/page.tsx
@@ -0,0 +1,52 @@
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import { login } from "./actions";
+
+export default function LoginPage() {
+  return (
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            formAction={login}
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
