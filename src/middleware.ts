764425c8e13393f412df48b1bcb10ef2a306a791
Logan Thimer <thimerlogan@gmail.com>
2024-06-20 06:59:00 -0600

    Added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { NextResponse, type NextRequest } from "next/server";
+// import { updateSession } from "./utils/supabase/middleware";
+import { updateSession } from "@/utils/supabase/middleware";
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+
+// export async function middleware(req: NextRequest) {
+//   const res = NextResponse.next();
+//   const supabase = createMiddlewareClient({ req, res });
+
+//   const {
+//     data: { session },
+//     error,
+//   } = await supabase.auth.getSession();
+
+//   if (!session) {
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
+
+//   return res;
+// }
+
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
