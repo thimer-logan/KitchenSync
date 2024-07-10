52607d9a501d187190503ab06dd029dc6c259256
Logan Thimer <thimerlogan@gmail.com>
2024-07-10 06:37:45 -0600

    Added spinner for loading icon

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,20 @@
+"use client";
+
 import React from "react";
+import { PacmanLoader } from "react-spinners";
 
 export default function Loading() {
-  return <div>loading</div>;
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <PacmanLoader color="#607D8B" />;
+    </div>
+  );
 }
