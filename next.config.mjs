fc5f4f23543d5807c55c44e5bcd2fe70116511c1
Logan Thimer <thimerlogan@gmail.com>
2024-07-28 10:39:58 -0600

    Modified middleware to redirect to login if not authenticated

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/login",
+        destination: "/a/storage",
         permanent: false,
       },
     ];
--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -54,7 +54,20 @@ export async function updateSession(request: NextRequest) {
     }
   );
 
-  await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith("/login") &&
+    !request.nextUrl.pathname.startsWith("/auth")
+  ) {
+    // no user, potentially respond by redirecting the user to the login page
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
 
   return response;
 }
